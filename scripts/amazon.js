cf81b3536e0bc69de06e31c0531d296c56b1a323
dulgx <dulguunnu@gmail.com>
2023-12-21 17:42:34 +0800

    javascript added

---
+++ scripts/amazon.js
@@ -0,0 +1,27 @@
+const products = [{
+    Image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    Name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating: {
+        stars: 4.5,
+        count: 87
+    },
+    priceCents: 1090
+},
+{
+    Image: 'images/products/intermediate-composite-basketball.jpg',
+    Name: 'Intermediate Size Basketball',
+    rating: {
+        stars: 4,
+        count: 127
+    },
+    priceCents: 2095
+}, {
+    Image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    Name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating: {
+        stars: 4.5,
+        count: 56
+    },
+    priceCents: 799
+}
+];
