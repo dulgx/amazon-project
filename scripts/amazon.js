bb8bfe0e682842c8c7e72afe76072af5668904da
dulgx <dulguunnu@gmail.com>
2023-12-22 10:59:25 +0800

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,44 +1,15 @@
-const products = [{
-    Image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    Name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-},
-{
-    Image: 'images/products/intermediate-composite-basketball.jpg',
-    Name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    Image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    Name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}
-];
-
 let productsHTML = '';
-
 
 products.forEach((product) => {
     productsHTML += `
         <div class="product-container">
             <div class="product-image-container">
                 <img class="product-image" 
-                    src="${product.Image}">
+                    src="${product.image}">
             </div>
 
             <div class="product-name limit-text-to-2-lines">
-                ${product.Name}
+                ${product.name}
             </div>
 
             <div class="product-rating-container">
